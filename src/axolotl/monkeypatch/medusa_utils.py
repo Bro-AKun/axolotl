473423f6b7550b1a43095d067d10775daaf9175b
Bro-AKun <158448654+Bro-AKun@users.noreply.github.com>
2025-10-24 02:14:45 +0800

    Update medusa_utils.py

--- src/axolotl/monkeypatch/medusa_utils.py
+++ src/axolotl/monkeypatch/medusa_utils.py
@@ -262,7 +262,7 @@ def add_medusa_heads(
                 # print("Number of layers:", len(all_layer_outputs))  # 打印层数
                 # for i, layer_output in enumerate(all_layer_outputs):
                 #     print(f"Layer {i} output shape:", layer_output.shape)
-            x = 30
+            x = 20
             #     # 1. 提取后x层的输出
             # last_x_layers = all_layer_outputs[-x:]  # 列表，包含x个 [1, 4096, 4096] 张量
 
